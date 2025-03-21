4895c083a7efec2e2e5e74600b47e2963dfddc1e
Ian Macartney <ianhmacartney@gmail.com>
2025-03-20 17:33:42 -0700

    also try ai [sdk] to verify it's only mastra

--- convex/regularRuntime.ts
+++ convex/regularRuntime.ts
@@ -1,3 +1,9 @@
 import { TABLE_EVALS } from "@mastra/core/storage";
 
+import * as sdk from "@ai-sdk/openai";
+export * as ai from "ai";
+export { sdk };
+
+export const o = sdk.openai("gpt-3.5-turbo");
+
 export { TABLE_EVALS };
